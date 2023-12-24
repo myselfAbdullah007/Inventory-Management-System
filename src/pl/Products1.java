d3d92836e53c5cdc569e6ca8137ffa51a19ccedb
EhsanTanvir444 <ehsantanvir13@gmail.com>
2023-12-24 12:33:04 +0500

    Completed Products CRUD

--- src/pl/Products1.java
+++ src/pl/Products1.java
@@ -6,9 +6,9 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import dal.ProductsDAO;
-
 
 /**
  * 
@@ -31,7 +31,7 @@ public class Products1 extends JFrame {
 		dal = new ProductsDAO();
 
 		setTitle("Products");
-		 setSize(800, 400);
+		setSize(800, 400);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLocationRelativeTo(null);
 
@@ -99,35 +99,32 @@ public class Products1 extends JFrame {
 }
 
 class Add extends JPanel {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	private JTextField nameField;
-	private JTextField quantityField;
-	private JTextField priceField;
-	private JTextField descriptionField;
-	private JTextField categoryField;
+	private JTextField nameField, quantityField, priceField, descriptionField, categoryField;
 	private JTextArea resultArea;
 
 	public Add(Products1 Presenter) {
-
 		setLayout(new BorderLayout());
 
 		JPanel inputPanel = new JPanel();
-		inputPanel.setLayout(new FlowLayout((FlowLayout.LEADING)));
+		inputPanel.setLayout(new GridLayout(6, 2, 5, 5));
 
 		JButton addButton = new JButton("ENTER");
+		addButton.setBackground(new Color(50, 205, 50));
+		addButton.setForeground(Color.WHITE);
+
 		JLabel nameLabel = new JLabel("PRODUCT NAME");
 		JLabel quantityLabel = new JLabel("QUANTITY");
 		JLabel priceLabel = new JLabel("PRICE");
 		JLabel descriptionLabel = new JLabel("DESCRIPTION");
 		JLabel categoryLabel = new JLabel("CATEGORY");
+
+		nameField = new JTextField(20);
 		quantityField = new JTextField(20);
 		priceField = new JTextField(20);
 		descriptionField = new JTextField(20);
 		categoryField = new JTextField(20);
-		nameField = new JTextField(20);
 
 		inputPanel.add(nameLabel);
 		inputPanel.add(nameField);
@@ -152,10 +149,10 @@ class Add extends JPanel {
 				try {
 					String name = nameField.getText();
 					Integer quantity = Integer.parseInt(quantityField.getText());
-					Float price =Float.parseFloat(priceField.getText());
+					Float price = Float.parseFloat(priceField.getText());
 					String description = descriptionField.getText();
 					String category = categoryField.getText();
-					Presenter.getBl().addProducts(name,quantity,price,description,category);
+					Presenter.getBl().addProducts(name, quantity, price, description, category);
 					resultArea.setText("ADDED");
 				} catch (NumberFormatException ex) {
 					resultArea.setText("Invalid input.");
@@ -164,6 +161,7 @@ class Add extends JPanel {
 				}
 			}
 		});
+
 		add(resultArea, BorderLayout.NORTH);
 	}
 }
@@ -177,158 +175,144 @@ class Read extends JPanel {
 		this.rootPresenter = Presenter;
 		setLayout(new BorderLayout());
 
-		// Creating the JTable and adding it to the panel
-		String[] columnNames = { "ProductName" , "Quantity" , "Price" ,"Description" , "Category"};
+		String[] columnNames = { "Product Name", "Quantity", "Price", "Description", "Category" };
 		Object[][] data = new Object[0][];
-		table = new JTable(data, columnNames) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-		};
+		table = new JTable(new DefaultTableModel(data, columnNames));
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setAutoCreateRowSorter(true);
+
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane, BorderLayout.CENTER);
 	}
 
 	public void refreshTable() {
-		List<Object[]> roots = rootPresenter.getBl().viewProducts();
-		Object[][] data = new Object[roots.size()][5];
-		for (int i = 0; i < roots.size(); i++) {
-			data[i] = roots.get(i);
+		List<Object[]> products = rootPresenter.getBl().viewProducts();
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
+
+		for (Object[] product : products) {
+			model.addRow(product);
 		}
 	}
 }
-// Class OF Update Root Functionality
-	class Update extends JPanel {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		private JTextField oldnameField;
-		private JTextField nameField;
-		private JTextField quantityField;
-		private JTextField priceField;
-		private JTextField descriptionField;
-		private JTextField categoryField;
-		private JTextArea resultArea;
 
-		public Update(Products1 Presenter) {
-			setLayout(new BorderLayout());
+class Update extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private JTextField oldnameField, nameField, quantityField, priceField, descriptionField, categoryField;
+	private JTextArea resultArea;
 
-			JPanel inputPanel = new JPanel();
-			inputPanel.setLayout(new FlowLayout((FlowLayout.RIGHT)));
+	public Update(Products1 Presenter) {
+		setLayout(new BorderLayout());
 
-			JButton updateButton = new JButton("ENTER");
-			JLabel oldnameLabel = new JLabel("OLD PRODUCT NAME");
-			JLabel nameLabel = new JLabel("UPDATED PRODUCT NAME");
-			JLabel quantityLabel = new JLabel("UPDATED QUANTITY");
-			JLabel priceLabel = new JLabel("UPDATED PRICE");
-			JLabel descriptionLabel = new JLabel("UPDATED DESCRIPTION");
-			JLabel categoryLabel = new JLabel("UPDATED CATEGORY");
-			quantityField = new JTextField(20);
-			priceField = new JTextField(20);
-			descriptionField = new JTextField(20);
-			categoryField = new JTextField(20);
-			nameField = new JTextField(20);
+		JPanel inputPanel = new JPanel();
+		inputPanel.setLayout(new GridLayout(7, 2, 5, 5)); // Improved layout manager
 
-			oldnameField = new JTextField(20);
-			nameField = new JTextField(20);
+		JButton updateButton = new JButton("UPDATE");
+		updateButton.setBackground(new Color(70, 130, 180)); // Blue background color
+		updateButton.setForeground(Color.WHITE); // White text color
 
-			inputPanel.add(oldnameLabel);
-			inputPanel.add(oldnameField);
-			inputPanel.add(nameLabel);
-			inputPanel.add(nameField);
-			inputPanel.add(quantityLabel);
-			inputPanel.add(quantityField);
-			inputPanel.add(priceLabel);
-			inputPanel.add(priceField);
-			inputPanel.add(descriptionLabel);
-			inputPanel.add(descriptionField);
-			inputPanel.add(categoryLabel);
-			inputPanel.add(categoryField);
-			inputPanel.add(updateButton);
+		JLabel oldnameLabel = new JLabel("Old Product Name");
+		JLabel nameLabel = new JLabel("Updated Product Name");
+		JLabel quantityLabel = new JLabel("Updated Quantity");
+		JLabel priceLabel = new JLabel("Updated Price");
+		JLabel descriptionLabel = new JLabel("Updated Description");
+		JLabel categoryLabel = new JLabel("Updated Category");
 
-			resultArea = new JTextArea(5, 10);
-			resultArea.setEditable(false);
+		// Styling text fields
+		oldnameField = new JTextField(20);
+		nameField = new JTextField(20);
+		quantityField = new JTextField(20);
+		priceField = new JTextField(20);
+		descriptionField = new JTextField(20);
+		categoryField = new JTextField(20);
 
-			add(inputPanel, BorderLayout.CENTER);
+		inputPanel.add(oldnameLabel);
+		inputPanel.add(oldnameField);
+		inputPanel.add(nameLabel);
+		inputPanel.add(nameField);
+		inputPanel.add(quantityLabel);
+		inputPanel.add(quantityField);
+		inputPanel.add(priceLabel);
+		inputPanel.add(priceField);
+		inputPanel.add(descriptionLabel);
+		inputPanel.add(descriptionField);
+		inputPanel.add(categoryLabel);
+		inputPanel.add(categoryField);
+		inputPanel.add(updateButton);
 
-			updateButton.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					try {
-						String oldname = oldnameField.getText();
-						String name = nameField.getText();
-						Integer quantity = Integer.parseInt(quantityField.getText());
-						Float price =Float.parseFloat(priceField.getText());
-						String description = descriptionField.getText();
-						String category = categoryField.getText();
+		resultArea = new JTextArea(5, 10);
+		resultArea.setEditable(false);
 
-						boolean success = Presenter.getBl().updateProducts(oldname,name,quantity,price,description,category);
-						if (success) {
-							resultArea.setText("UPDATED SUCCESSFULLY");
-						} else {
-							resultArea.setText("NOT UPDATED SUCCESSFULLY");
-						}
-					} catch (NumberFormatException ex) {
-						resultArea.setText("Invalid input.");
-					} catch (Exception ex) {
-						resultArea.setText("An error occurred.");
-					}
+		add(inputPanel, BorderLayout.CENTER);
+
+		updateButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String oldname = oldnameField.getText();
+					String name = nameField.getText();
+					Integer quantity = Integer.parseInt(quantityField.getText());
+					Float price = Float.parseFloat(priceField.getText());
+					String description = descriptionField.getText();
+					String category = categoryField.getText();
+
+					boolean success = Presenter.getBl().updateProducts(oldname, name, quantity, price, description,
+							category);
+					resultArea.setText(success ? "Updated Successfully" : "Not Updated Successfully");
+				} catch (NumberFormatException ex) {
+					resultArea.setText("Invalid input. Please check your input values.");
+				} catch (Exception ex) {
+					resultArea.setText("An error occurred. Please try again later.");
 				}
-			});
+			}
+		});
 
-			add(resultArea, BorderLayout.NORTH);
-		}
+		add(resultArea, BorderLayout.NORTH);
 	}
+}
 
-	class Delete extends JPanel {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		private JTextField nameField;
-		private JTextArea resultArea;
+class Delete extends JPanel {
+    private static final long serialVersionUID = 1L;
+    private JTextField nameField;
+    private JTextArea resultArea;
 
-		public Delete(Products1 Presenter) {
-			setLayout(new BorderLayout());
+    public Delete(Products1 Presenter) {
+        setLayout(new BorderLayout());
 
-			JPanel inputPanel = new JPanel();
-			inputPanel.setLayout(new FlowLayout((FlowLayout.LEADING)));
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(2, 2, 5, 5)); // Improved layout manager
 
-			JButton deleteButton = new JButton("ENTER");
-			JLabel nameLabel = new JLabel("ENTER PRODUCT NAME");
+        JButton deleteButton = new JButton("DELETE");
+        deleteButton.setBackground(new Color(220, 20, 60)); // Crimson background color
+        deleteButton.setForeground(Color.WHITE); // White text color
 
-			nameField = new JTextField(20);
+        JLabel nameLabel = new JLabel("Product Name to Delete");
 
-			inputPanel.add(nameLabel);
-			inputPanel.add(nameField);
-			inputPanel.add(deleteButton);
+        // Styling text fields
+        nameField = new JTextField(20);
 
-			resultArea = new JTextArea(5, 10);
-			resultArea.setEditable(false);
+        inputPanel.add(nameLabel);
+        inputPanel.add(nameField);
+        inputPanel.add(deleteButton);
 
-			add(inputPanel, BorderLayout.CENTER);
+        resultArea = new JTextArea(5, 10);
+        resultArea.setEditable(false);
 
-			deleteButton.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					try {
-						String name = nameField.getText();
-						boolean success = Presenter.getBl().deleteProducts(name);
-						if (success) {
-							resultArea.setText("DELETED SUCCESSFULLY");
-						} else {
-							resultArea.setText("NOT DELETED SUCCESSFULLY");
-						}
-					} catch (NumberFormatException ex) {
-						resultArea.setText("Invalid input.");
-					} catch (Exception ex) {
-						resultArea.setText("An error occurred.");
-					}
-				}
-			});
+        add(inputPanel, BorderLayout.CENTER);
 
-			add(resultArea, BorderLayout.NORTH);
-		}
-	}
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String name = nameField.getText();
+                    boolean success = Presenter.getBl().deleteProducts(name);
+                    resultArea.setText(success ? "Deleted Successfully" : "Not Deleted Successfully");
+                } catch (Exception ex) {
+                    resultArea.setText("An error occurred. Please try again later.");
+                }
+            }
+        });
+
+        add(resultArea, BorderLayout.NORTH);
+    }
+}
