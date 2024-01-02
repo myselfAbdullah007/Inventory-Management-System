1caac340e5fa4d36f52c438ba6ace0cc8e51118c
Hasan-Mamoon <mianhassan085@gmail.com>
2024-01-03 01:37:46 +0500

    Added Methods to Product DTStock Report Generation

--- src/pl/Reports.java
+++ src/pl/Reports.java
@@ -1,17 +1,23 @@
 package pl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
+import DTO.Product;
+import dal.ProductDAO;
+
 public class Reports {
 
     public static void main(String[] args) {
         try {
-            createPdfWithTable("example.pdf");
+            createPdfWithTable("Stocks.pdf");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -23,17 +29,18 @@ public class Reports {
             document.addPage(page);
 
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                float margin = 50;
+                ProductDAO prod = new ProductDAO();
+                List<Product> prodlist = prod.getAllProducts();
+            	float margin = 50;
                 float yStart = page.getMediaBox().getHeight() - margin;
                 float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
                 float yPosition = yStart;
-                float bottomMargin = 70;
 
                 drawTableHeader(contentStream, margin, yStart, tableWidth);
                 yPosition -= 15;
 
-                for (int i = 0; i < 10; i++) {
-                    drawTableRow(contentStream, margin, yPosition, tableWidth, "Product " + (i + 1), String.valueOf(i + 1));
+                for (Product p:prodlist) {
+                    drawTableRow(contentStream, margin, yPosition, tableWidth, p.getProductId(), p.getName(),p.getQunatity());
                     yPosition -= 15;
                 }
             }
@@ -46,19 +53,24 @@ public class Reports {
         contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
         contentStream.beginText();
         contentStream.newLineAtOffset(margin, yStart);
-        contentStream.showText("Product");
+        contentStream.showText("ProductID");
         contentStream.newLineAtOffset(tableWidth * 0.4f, 0);
-        contentStream.showText("Quantity");
+        contentStream.showText("Name");
+        contentStream.newLineAtOffset(tableWidth * 0.4f, 0);
+        contentStream.showText("Qunatity");
         contentStream.endText();
     }
 
-    private static void drawTableRow(PDPageContentStream contentStream, float margin, float yPosition, float tableWidth, String product, String quantity) throws IOException {
+    private static void drawTableRow(PDPageContentStream contentStream, float margin, float yPosition, float tableWidth, String product, String name,String quantity) throws IOException {
         contentStream.setFont(PDType1Font.HELVETICA, 12);
         contentStream.beginText();
         contentStream.newLineAtOffset(margin, yPosition);
         contentStream.showText(product);
         contentStream.newLineAtOffset(tableWidth * 0.4f, 0);
+        contentStream.showText(name);
+        contentStream.newLineAtOffset(tableWidth * 0.4f, 0);
         contentStream.showText(quantity);
         contentStream.endText();
     }
+    
 }
